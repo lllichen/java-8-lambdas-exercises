12647ddb3280a88a67188e168c1e430fb7806f02
lichen <lichen@daojia.com>
2018-03-08 14:39:14 +0800

    add info

--- src/main/java/com/insightfullogic/java8/my/exercise/ExerciseMap.java
+++ src/main/java/com/insightfullogic/java8/my/exercise/ExerciseMap.java
@@ -1,7 +1,12 @@
 package com.insightfullogic.java8.my.exercise;
 
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
+import java.util.stream.Stream;
+
+import static java.util.Arrays.asList;
+import static java.util.stream.Collectors.toList;
 
 /**
  * Created by lichen@daojia.com on 2018-3-8.
@@ -10,7 +15,33 @@ public class ExerciseMap {
 
 
     public static void main(String[] args) {
-        List<Integer> list = Arrays.asList(1,2,3,4,5,6,7,8,9,10,11,12,13,14,15);
+
+        List<Integer> list = asList(1,2,3,4,5,6,7,8,9,10,11,12,13,14,15);
         list.stream().map( value->String.valueOf( value )+"a" ).forEach( value-> System.out.println(value) );
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        List<String> collected = new ArrayList<>();
+        for (String string : asList("a", "b", "hello")) {
+            String uppercaseString = string.toUpperCase();
+            collected.add(uppercaseString);
+        }
+
+        List<String> newCollected = Stream.of("a", "b", "hello")
+                .map(string -> string.toUpperCase())
+                .collect(toList());
+
+
     }
 }
